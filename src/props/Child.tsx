47fb9aaf4ceace50cf29e3f0dd39a881246ac6e1
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-21 23:34:42 +0700

    explicity props type

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,6 +2,13 @@ interface ChildProps {
   color: string;
 }
 
-export const Child = ({ color }: ChildProps) => {
+/**
+ * Child will be a React function component
+ * Child might have properties assgned to it like propTypes and contextTypes
+ * Child will receive props of type 'ChildProps'
+ * @param param0 color props is passing from parent component
+ * @returns React Functional Component
+ */
+export const Child: React.FC<ChildProps> = ({ color }) => {
   return <h1>{color}</h1>;
 };
