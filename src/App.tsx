f4ce6e316b89563a38cabba5e2d7de4c257d7252
VanTruongUIT <tvtruong.uit@gmail.com>
2023-06-23 23:50:00 +0700

    ts with state type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Parent from "./props/Parent";
 import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 function App() {
   return (
     <div className="App">
       <h1>React 18 Alpha Demo</h1>
-      <GuestList />
+
+      <UserSearch />
+      {/* <GuestList /> */}
       {/* <Parent /> */}
     </div>
   );
